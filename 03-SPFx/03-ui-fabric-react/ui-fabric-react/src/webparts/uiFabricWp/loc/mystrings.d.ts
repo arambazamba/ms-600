83cc7dfdfdee05eca0b1135641ff65d781702c81
Alexander Pajer <alexander.pajer@integrations.at>
2020-10-04 19:59:32 +0200

    labs

---
+++ 03-SPFx/03-ui-fabric-react/ui-fabric-react/src/webparts/uiFabricWp/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IUiFabricWpWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'UiFabricWpWebPartStrings' {
+  const strings: IUiFabricWpWebPartStrings;
+  export = strings;
+}
