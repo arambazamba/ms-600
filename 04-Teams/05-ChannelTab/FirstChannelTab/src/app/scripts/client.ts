091e7329393396500096ad5d7280f5391c3b34c6
Alexander Pajer <alexander.pajer@integrations.at>
2020-10-31 17:33:17 +0100

    restructure

--- 04-Teams/05-ChannelTab/FirstChannelTab/src/app/scripts/client.ts
+++
@@ -1,4 +0,0 @@
-// Automatically added for the firstMathTab tab
-export * from "./firstMathTab/FirstMathTab";
-export * from "./firstMathTab/FirstMathTabConfig";
-export * from "./firstMathTab/FirstMathTabRemove";
