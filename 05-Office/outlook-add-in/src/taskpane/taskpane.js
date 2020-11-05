0a9f6ee18d5e826ef2c04973d655bc612159e75f
Alexander Pajer <alexander.pajer@integrations.at>
2020-11-05 18:25:14 +0100

    get item

--- 05-Office/outlook-add-in/src/taskpane/taskpane.js
+++ 05-Office/outlook-add-in/src/taskpane/taskpane.js
@@ -6,21 +6,33 @@ Office.onReady(info => {
   if (info.host === Office.HostType.Outlook) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
+    document.getElementById("btnRun").onclick = getAttachments;
+    document.getElementById("btnSave").onclick = saveToAzure;
   }
 });
 
-export async function run() {
+export async function getAttachments() {
   // Get a reference to the current message
   var item = Office.context.mailbox.item;
   var html = "<div>";
 
   item.attachments.forEach(at => {
-    html += `<div><input type="checkbox" id="scales" name="scales">  ${at.name}</div>`;
+    html += `<div><input type="checkbox" name="${at.id}">${at.name}</div>`;
   });
 
   html += "</div>";
 
   // Write message property value to the task pane
   document.getElementById("item-subject").innerHTML = html;
+}
+
+export function saveToAzure() {
+  const checks = document.querySelectorAll("input[type='checkbox']");
+
+  checks.forEach(chk => {
+    if (chk.checked) {
+      let item = Office.context.mailbox.item.attachments.find(i => i.id == chk.name);
+      console.log("saving to azure", item);
+    }
+  });
 }
--- 05-Office/readme.md
+++ 05-Office/readme.md
@@ -18,6 +18,8 @@ $ npm install -g yo generator-office
 
 [Persisting add-in state and settings](https://docs.microsoft.com/en-us/office/dev/add-ins/develop/persisting-add-in-state-and-settings)
 
+[Sideload Outlook add-ins for testing](https://docs.microsoft.com/en-us/office/dev/add-ins/outlook/sideload-outlook-add-ins-for-testing#:~:text=Sideload%20an%20add%2Din%20in%20classic%20Outlook%20on%20the%20web,-Go%20to%20Outlook&text=On%20the%20Manage%20add%2Dins,add%2Din%20and%20install%20it.)
+
 ## Actionable Messages
 
 [Actionable Message Designer](https://amdesigner.azurewebsites.net)
