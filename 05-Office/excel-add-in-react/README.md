3fd840513a1700f28b1741ce23b4d51f5bd0d63f
Alexander Pajer <alexander.pajer@integrations.at>
2020-11-05 19:04:28 +0100

    excel

---
+++ 05-Office/excel-add-in-react/README.md
@@ -0,0 +1,15 @@
+# Excel Portfolio - React
+
+To run this demo, open a terminal/command prompt, and change directories to this location, and run the following commands
+
+```shell
+npm install
+npm run build
+npm run dev-server
+```
+
+Then follow one of these methods to sideload and test the Office Add-in.
+
+* Windows: [Sideload Office Add-ins on Windows](https://docs.microsoft.com/en-us/office/dev/add-ins/testing/create-a-network-shared-folder-catalog-for-task-pane-and-content-add-ins)
+* Word Online: [Sideload Office Add-ins in Office Online](https://docs.microsoft.com/en-us/office/dev/add-ins/testing/sideload-office-add-ins-for-testing#sideload-an-office-add-in-on-office-online)
+* iPad and Mac: [Sideload Office Add-ins on iPad and Mac](https://docs.microsoft.com/en-us/office/dev/add-ins/testing/sideload-an-office-add-in-on-ipad-and-mac)
---
+++ 05-Office/excel-add-in-react/src/taskpane/index.tsx
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+import "office-ui-fabric-react/dist/css/fabric.min.css";
+import App from "./components/App";
+import { AppContainer } from "react-hot-loader";
+import { initializeIcons } from "office-ui-fabric-react/lib/Icons";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+/* global AppCpntainer, Component, document, Office, module, require */
+
+initializeIcons();
+
+let isOfficeInitialized = false;
+
+const title = 'Excel Portfolio';
+
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component title={title} isOfficeInitialized={isOfficeInitialized} />
+    </AppContainer>,
+    document.getElementById("container")
+  );
+};
+
+/* Render application after Office initializes */
+Office.initialize = () => {
+  isOfficeInitialized = true;
+  render(App);
+};
+
+/* Initial render showing a progress bar */
+render(App);
+
+if ((module as any).hot) {
+  (module as any).hot.accept("./components/App", () => {
+    const NextApp = require("./components/App").default;
+    render(NextApp);
+  });
+}
