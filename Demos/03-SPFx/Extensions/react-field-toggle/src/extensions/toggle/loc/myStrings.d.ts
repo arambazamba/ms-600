47116247a58b0b67ed52c1678869c9060964975d
Alexander Pajer <alexander.pajer@integrations.at>
2021-03-20 18:02:40 +0100

    restructure

--- Demos/03-SPFx/Extensions/react-field-toggle/src/extensions/toggle/loc/myStrings.d.ts
+++
@@ -1,8 +0,0 @@
-declare interface IToggleFieldCustomizerStrings {
-  Title: string;
-}
-
-declare module 'ToggleFieldCustomizerStrings' {
-  const strings: IToggleFieldCustomizerStrings;
-  export = strings;
-}
