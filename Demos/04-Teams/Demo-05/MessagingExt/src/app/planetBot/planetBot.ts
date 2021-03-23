90c6a3010f8cbfe7784daa6f6d5b3031655e9b82
Alexander Pajer <alexander.pajer@integrations.at>
2021-03-23 21:00:14 +0100

    app update

--- Demos/04-Teams/Demo-05/MessagingExt/src/app/planetBot/planetBot.ts
+++ Demos/04-Teams/Demo-05/MessagingExt/src/app/planetBot/planetBot.ts
@@ -12,7 +12,6 @@ import { find, sortBy } from "lodash";
 
 import * as Util from "util";
 const TextEncoder = Util.TextEncoder;
-
 import * as debug from "debug";
 const log = debug("msteams");
 
@@ -21,10 +20,7 @@ export class PlanetBot extends TeamsActivityHandler {
     super();
   }
 
-  protected handleTeamsMessagingExtensionFetchTask(
-    context: TurnContext,
-    action: MessagingExtensionAction
-  ): Promise<MessagingExtensionActionResponse> {
+  protected handleTeamsMessagingExtensionFetchTask(context: TurnContext, action: MessagingExtensionAction): Promise<MessagingExtensionActionResponse> {
     // load planets & sort them by their order from the sun
     const planets: any = require("./planets.json");
     const sortedPlanets: any = sortBy(planets, ["id"]).map((planet) => {
@@ -57,18 +53,13 @@ export class PlanetBot extends TeamsActivityHandler {
     return Promise.resolve(response);
   }
 
-  protected handleTeamsMessagingExtensionSubmitAction(
-    context: TurnContext,
-    action: MessagingExtensionAction
-  ): Promise<MessagingExtensionActionResponse> {
+  protected handleTeamsMessagingExtensionSubmitAction(context: TurnContext, action: MessagingExtensionAction): Promise<MessagingExtensionActionResponse> {
     switch (action.commandId) {
       case "planetExpanderAction":
         // load planets
         const planets: any = require("./planets.json");
         // get the selected planet
-        const selectedPlanet: any = planets.filter(
-          (planet) => planet.id === action.data.planetSelector
-        )[0];
+        const selectedPlanet: any = planets.filter((planet) => planet.id === action.data.planetSelector)[0];
         const adaptiveCard = this.getPlanetDetailCard(selectedPlanet);
 
         // generate the response
@@ -85,24 +76,19 @@ export class PlanetBot extends TeamsActivityHandler {
     }
   }
 
-  private getPlanetDetailCard(
-    selectedPlanet: any
-  ): MessagingExtensionAttachment {
+  private getPlanetDetailCard(selectedPlanet: any): MessagingExtensionAttachment {
     // load display card
     const adaptiveCardSource: any = require("./planetDisplayCard.json");
 
     // update planet fields in display card
     adaptiveCardSource.actions[0].url = selectedPlanet.wikiLink;
-    find(adaptiveCardSource.body, { id: "cardHeader" }).items[0].text =
-      selectedPlanet.name;
+    find(adaptiveCardSource.body, { id: "cardHeader" }).items[0].text = selectedPlanet.name;
     const cardBody: any = find(adaptiveCardSource.body, { id: "cardBody" });
     find(cardBody.items, { id: "planetSummary" }).text = selectedPlanet.summary;
-    find(cardBody.items, { id: "imageAttribution" }).text =
-      "*Image attribution: " + selectedPlanet.imageAlt + "*";
+    find(cardBody.items, { id: "imageAttribution" }).text = "*Image attribution: " + selectedPlanet.imageAlt + "*";
     const cardDetails: any = find(cardBody.items, { id: "planetDetails" });
     cardDetails.columns[0].items[0].url = selectedPlanet.imageLink;
-    find(cardDetails.columns[1].items[0].facts, { id: "orderFromSun" }).value =
-      selectedPlanet.id;
+    find(cardDetails.columns[1].items[0].facts, { id: "orderFromSun" }).value = selectedPlanet.id;
     find(cardDetails.columns[1].items[0].facts, {
       id: "planetNumSatellites",
     }).value = selectedPlanet.numSatellites;
