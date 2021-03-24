30d5a735143a62a0cc1413d3213322f52d6aa33d
Alexander Pajer <alexander.pajer@integrations.at>
2021-03-24 08:02:30 +0100

    Utils

---
+++ Demos/03-SPFx/WebParts/13-Utils/spfx-utils/src/webparts/utilsWp/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IUtilsWpWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'UtilsWpWebPartStrings' {
+  const strings: IUtilsWpWebPartStrings;
+  export = strings;
+}
