ff5d9a664de19ad7c274c9a1af3d58064042be95
Alexander Pajer <alexander.pajer@integrations.at>
2021-06-11 20:12:45 +0200

    ng update

--- Demos/01-Identity/02-MSAL/02-Auth-Flows/01-spa/01-ng-msal-app/src/polyfills.ts
+++ Demos/01-Identity/02-MSAL/02-Auth-Flows/01-spa/01-ng-msal-app/src/polyfills.ts
@@ -56,7 +56,7 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'core-js'; // Needed for IE11
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 
 
 /***************************************************************************************************
