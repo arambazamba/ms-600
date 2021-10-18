34feeea7ce9cef706baa78d1bc2e09cedc95cacf
Alexander Pajer <alexander.pajer@integrations.at>
2021-10-18 09:27:32 +0200

    labvm

--- Setup/create-lab-vm.sh
+++ Setup/create-lab-vm.sh
@@ -7,6 +7,6 @@ pwd=Lab@dmin1233
 
 az group create -n $grp -l $loc
 
-az vm create -g $grp -n $vmname --admin-username $user --admin-password $pwd --image  MicrosoftWindowsDesktop:Windows-10:21h1-pro-g2:19043.985.2105141120 --size Standard_E2s_v3
+az vm create -g $grp -n $vmname --admin-username $user --admin-password $pwd --image  MicrosoftWindowsDesktop:Windows-10:21h1-pro:19043.1237.2109130412 --size Standard_E2s_v3
 
 az vm auto-shutdown -g $grp -n $vmname --time 1830
