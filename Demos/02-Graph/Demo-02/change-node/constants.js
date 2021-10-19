5d2d28165bf5bbd3978f8872f5294b3762e09843
Alexander Pajer <alexander.pajer@integrations.at>
2021-10-19 09:40:29 +0200

    lab-update

--- Demos/02-Graph/Demo-02/change-node/constants.js
+++ Demos/02-Graph/Demo-02/change-node/constants.js
@@ -2,7 +2,7 @@
 exports.msalConfiguration = {
   authority: "https://login.microsoftonline.com/common",
   clientID: "7adb8d94-4233-4f7c-9563-87bdeb7ad27a",
-  clientSecret: "vNz.V6ZbQ2rGVi7_7T~_lm.NjQLO~158rK",
+  clientSecret: "RTU7Q~Qedv2MD7xLzRn3Ea6VW3Khhg3q6jQon",
   tenantID: "d92b247e-90e0-4469-a129-6a32866c0d0a",
   redirectUri: "http://localhost:3000/callback",
 };
