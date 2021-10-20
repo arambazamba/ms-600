df806590366aa2062244d609086dce227b6505a3
Alexander Pajer <alexander.pajer@integrations.at>
2021-10-20 20:38:58 +0200

    math-tab-yeoman

---
+++ Demos/04-Teams/Demo-03/MathTab/src/server/TeamsAppsComponents.ts
@@ -0,0 +1,6 @@
+// Components will be added here
+export const nonce = {}; // Do not remove!
+// Automatically added for the staticTab tab
+export * from "./staticTab/StaticTab";
+// Automatically added for the mathTab tab
+export * from "./mathTab/MathTab";
