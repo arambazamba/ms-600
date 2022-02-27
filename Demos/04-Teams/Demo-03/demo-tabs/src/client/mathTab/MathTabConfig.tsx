711eb9ad4ce7e1723f89c195c9bef4f5f2649986
Alexander Pajer <alexander.pajer@integrations.at>
2022-02-27 20:08:58 +0100

    all

--- Demos/04-Teams/Demo-03/demo-tabs/src/client/mathTab/MathTabConfig.tsx
+++ Demos/04-Teams/Demo-03/demo-tabs/src/client/mathTab/MathTabConfig.tsx
@@ -1,62 +1,64 @@
-import * as React from "react";
-import { Provider, Flex, Header, Input } from "@fluentui/react-northstar";
-import { useState, useEffect, useRef } from "react";
-import { useTeams } from "msteams-react-base-component";
-import * as microsoftTeams from "@microsoft/teams-js";
+import * as React from 'react';
+import { Provider, Flex, Header, Input, DropdownProps, Dropdown } from "@fluentui/react-northstar";
+import { useState, useEffect, useRef } from 'react';
+import { useTeams } from 'msteams-react-base-component';
+import * as microsoftTeams from '@microsoft/teams-js';
 
-/**
- * Implementation of MathTab configuration page
- */
+export interface IConfigMathTabState {
+    mathOperator?: string;
+    operandA: number;
+    operandB: number;
+    result: string;
+}
+
+// Implementation of MathTab configuration page
 export const MathTabConfig = () => {
-
     const [{ inTeams, theme, context }] = useTeams({});
-    const [text, setText] = useState<string>();
-    const entityId = useRef("");
+    const [mathOperator, setMathOperator] = useState<string>();
+    const entityId = useRef('');
 
     const onSaveHandler = (saveEvent: microsoftTeams.settings.SaveEvent) => {
-        const host = "https://" + window.location.host;
+        const host = 'https://' + window.location.host;
         microsoftTeams.settings.setSettings({
-            contentUrl: host + "/mathTab/?name={loginHint}&tenant={tid}&group={groupId}&theme={theme}",
-            websiteUrl: host + "/mathTab/?name={loginHint}&tenant={tid}&group={groupId}&theme={theme}",
-            suggestedDisplayName: "MathTab",
-            removeUrl: host + "/mathTab/remove.html?theme={theme}",
-            entityId: entityId.current
+            contentUrl: host + '/mathTab/?name={loginHint}&tenant={tid}&group={groupId}&theme={theme}',
+            websiteUrl: host + '/mathTab/?name={loginHint}&tenant={tid}&group={groupId}&theme={theme}',
+            suggestedDisplayName: 'MathTab',
+            removeUrl: host + '/mathTab/remove.html?theme={theme}',
+            entityId: entityId.current,
         });
         saveEvent.notifySuccess();
     };
 
     useEffect(() => {
         if (context) {
-            setText(context.entityId);
-            entityId.current = context.entityId;
-            microsoftTeams.settings.registerOnSaveHandler(onSaveHandler);
-            microsoftTeams.settings.setValidityState(true);
-            microsoftTeams.appInitialization.notifySuccess();
-        }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [context]);
+          setMathOperator(context.entityId.replace("MathTab", ""));
+          entityId.current = context.entityId;
+          microsoftTeams.settings.registerOnSaveHandler(onSaveHandler);
+          microsoftTeams.settings.setValidityState(true);
+          microsoftTeams.appInitialization.notifySuccess();
+          }
+          // eslint-disable-next-line react-hooks/exhaustive-deps
+        }, [context]);
 
-    return (
-        <Provider theme={theme}>
-            <Flex fill={true}>
-                <Flex.Item>
-                    <div>
-                        <Header content="Configure your tab" />
-                        <Input
-                            placeholder="Enter a value here"
-                            fluid
-                            clearable
-                            value={text}
-                            onChange={(e, data) => {
-                                if (data) {
-                                    setText(data.value);
-                                    entityId.current = data.value;
-                                }
-                            }}
-                            required />
-                    </div>
-                </Flex.Item>
-            </Flex>
-        </Provider>
-    );
+        return (
+            <Provider theme={theme}>
+              <Flex gap="gap.smaller" style={{ height: "300px" }}>
+                <Dropdown placeholder="Select the math operator"
+                  items={[
+                    "add",
+                    "subtract",
+                    "multiply",
+                    "divide"
+                  ]}
+                  onChange={(e, data) => {
+                    if (data) {
+                      const op = (data.value) ? data.value.toString() : "add";
+                      setMathOperator(op);
+                      entityId.current = `${op}MathPage`;
+                    }
+                  }}
+                  value={mathOperator}></Dropdown>
+              </Flex>
+            </Provider>
+          );
 };
