db508ad0edb1992bda7ad4d3d705fa67c36c7e68
Alexander Pajer <alexander.pajer@integrations.at>
2022-02-27 21:30:46 +0100

    tabs

---
+++ Demos/04-Teams/Demo-03/demo-tabs/src/server/TeamsAppsComponents.ts
@@ -0,0 +1,6 @@
+// Components will be added here
+export const nonce = {}; // Do not remove!
+// Automatically added for the staticTab tab
+export * from "./staticTab/StaticTab";
+// Automatically added for the configMathTab tab
+export * from "./configMathTab/ConfigMathTab";
