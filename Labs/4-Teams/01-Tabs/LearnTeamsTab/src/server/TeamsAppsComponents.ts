67a6445596753318ac2d21e05c0c148df583e9df
Alexander Pajer <alexander.pajer@integrations.at>
2022-02-27 11:07:52 +0100

    lab-update

--- Labs/4-Teams/01-Tabs/LearnTeamsTab/src/server/TeamsAppsComponents.ts
+++
@@ -1,4 +0,0 @@
-// Components will be added here
-export const nonce = {}; // Do not remove!
-// Automatically added for the learnTeamsTab tab
-export * from "./learnTeamsTab/LearnTeamsTab";
--- Labs/4-Teams/01-Tabs/LearnTeamsTab/src/server/learnTeamsTab/LearnTeamsTab.ts
+++
@@ -1,8 +0,0 @@
-import { PreventIframe } from "express-msteams-host";
-
-/**
- * Used as place holder for the decorators
- */
-@PreventIframe("/learnTeamsTab/index.html")
-export class LearnTeamsTab {
-}
