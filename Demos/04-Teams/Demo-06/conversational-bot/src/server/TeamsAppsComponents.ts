3c887ef01ad047d8e4b8d08df1ab0bb2f312e88f
Alexander Pajer <alexander.pajer@integrations.at>
2022-03-02 08:25:35 +0100

    bots working

---
+++ Demos/04-Teams/Demo-06/conversational-bot/src/server/TeamsAppsComponents.ts
@@ -0,0 +1,4 @@
+// Components will be added here
+export const nonce = {}; // Do not remove!
+// Automatically added for the conversationalbot19341Bot bot
+export * from "./conversationalbot19341Bot/Conversationalbot19341";
